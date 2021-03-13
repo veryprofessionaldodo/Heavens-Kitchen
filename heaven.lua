dd50005dbb0836a56a073aa6bca91e46b92aca69
zé pedro <ze_pedro_borges@hotmail.com>
2021-03-13 09:53:07 +0000

    started state machine

---
+++ heaven.lua
@@ -0,0 +1,78 @@
+-- title:  game title
+-- author: game developer
+-- desc:   short description
+-- script: lua
+
+t=0
+x=96
+y=24
+
+states = {
+	MAIN_MENU = 'menu',
+	LEVEL_ONE = 'level_one',
+	LEVEL_TWO = 'level_two',
+	LEVEL_THREE = 'level_three'
+}
+
+CURR_STATE = states.MAIN_MENU
+
+events = {
+	MAIN_MENU = 'main',
+	START_GAME = 'start',
+	NEXT_LEVEL = 'next',
+	LOST_GAME = 'lost',
+	WON_GAME = 'won'
+}
+
+function update_state_machine(event)
+	if event == events.MAIN_MENU then
+		CURR_STATE = states.MAIN_MENU
+	elseif event == events.START_GAME then
+		CURR_STATE = states.LEVEL_ONE
+	end
+end
+
+function init()
+	update_state_machine(events.MAIN_MENU)
+end
+
+init()
+
+function TIC()
+	
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+	cls(13)
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
